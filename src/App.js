44fe1c71cafb52f92416a77095087119d90e9701
Ankit Gahukar <topdacankit96@gmail.com>
2023-02-13 22:24:09 +0530

    add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Tempapp from './components/Tempapp';
 
 function App() {
   return (
    <div>
     <h1>Ankit</h1>
+    <Tempapp />
    </div>
   );
 }
---
+++ src/components/Tempapp.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Tempapp=()=>{
+    return(
+        <>
+        <h1>Gahukar</h1>
+        </>
+    )
+}
+export default Tempapp;
