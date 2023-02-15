b632350457148fd9fb40f0e66aa04a8cfc56b488
Ankit Gahukar <topdacankit96@gmail.com>
2023-02-15 16:13:21 +0530

    update

--- src/components/Tempapp.js
+++ src/components/Tempapp.js
@@ -7,7 +7,7 @@ const Tempapp=()=>{
 
     useEffect(()=>{
         const fetchApi= async()=>{
-            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=2f03a6c5491781e58e7d919d43cf1ce8`
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=2f03a6c5491781e58e7d919d43cf1ce8`
             const response = await fetch(url);
             console.log(response);
             const resJson = await response.json();
@@ -38,8 +38,8 @@ const Tempapp=()=>{
                 <h2 className="location">
                 <i className="fa-solid fa-street-view"></i>{search}
                 </h2>
-                <h1 className="temp">{city.temp}</h1>
-                <h4 className="tempMin_Max">{city.temp_max}|| {city.temp_min}</h4>
+                <h1 className="temp">{city.temp}°Cel</h1>
+                <h4 className="tempMin_Max">{city.temp_max}°Cel || {city.temp_min}°Cel</h4>
             </div>
            
             )
